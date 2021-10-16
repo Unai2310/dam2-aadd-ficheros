dd7c5b4e733d6fa804e5b955f7279cd46b412ffc
Unai2310 <78329457+Unai2310@users.noreply.github.com>
2021-10-16 17:50:05 +0200

    Actividad3

--- src/DirectoriosJava/DirectoriosJava/DirectoriosJson.java
+++ src/DirectoriosJava/DirectoriosJava/DirectoriosJson.java
@@ -1,43 +1,61 @@
 package DirectoriosJava;
 
 import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-public class Prueba {
+public class DirectoriosJson {
     public static void main(String[] args) {
-    {
-        Path ruta;
 
-    try {
+        File archivo = new File ("E:\\Documentos\\Github\\Acceso a datos\\dam2-aadd-ficheros\\src\\Config.json");
 
-    JsonParser parser = new JsonParser();
-    Object object =  parser.parse(new FileReader("Configuracion.json"));
-    JsonObject config = (JsonObject) object;
-    File fichero1= new File(config.get("carpeta1").toString());
-    fichero.createNewFile();
-    File fichero2= new File(config.get("carpeta2").toString());
-    fichero.createNewFile();
-    File fichero3= new File(config.get("carpeta3").toString());
-    fichero.createNewFile();
-    File fichero4= new File(config.get("carpeta4").toString());
-    fichero.createNewFile();
-    File fichero5= new File(config.get("carpeta5").toString());
-    fichero.createNewFile();
-    File fichero6= new File(config.get("carpeta6").toString());
-    fichero.createNewFile();
-    File fichero7= new File(config.get("carpeta7").toString());
-    fichero.createNewFile();
-    File ficher8= new File(config.get("carpeta8").toString());
-    fichero.createNewFile();
-    }catch(Exception e) {
-        System.err.println("Malo");
+        String prueba="";
+
+        try {
+            prueba = new String (Files.readAllBytes(archivo.toPath()), StandardCharsets.UTF_8);
+            System.out.println(prueba);
+        } catch (Exception e) {
+
+        }
+
+        JsonObject archivojson = JsonParser.parseString(prueba).getAsJsonObject();
+
+        String algo = archivojson.get("ruta1").getAsString();
+        File archivo2 = new File(algo);
+        archivo2.mkdirs();
+
+        String algo1 = archivojson.get("ruta2").getAsString();
+        archivo2 = new File(algo1);
+        archivo2.mkdirs();
+
+        String algo2 = archivojson.get("ruta3").getAsString();
+        archivo2 = new File(algo2);
+        archivo2.mkdirs();
+
+        String algo3 = archivojson.get("ruta4").getAsString();
+        archivo2 = new File(algo3);
+        archivo2.mkdirs();
+
+        String algo4 = archivojson.get("ruta5").getAsString();
+        archivo2 = new File(algo4);
+        archivo2.mkdirs();
+
+        String algo5 = archivojson.get("ruta6").getAsString();
+        archivo2 = new File(algo5);
+        archivo2.mkdirs();
+
+        String algo6 = archivojson.get("ruta7").getAsString();
+        archivo2 = new File(algo6);
+        archivo2.mkdirs();
+
+        String algo7 = archivojson.get("ruta8").getAsString();
+        archivo2 = new File(algo7);
+        archivo2.mkdirs();
+
     }
+}
 
-
-}
-}
-}
